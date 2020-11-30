f16c8e09235c1f0916ad5e270639320ffdba00f8
mmgitta <madan.gitta@gmail.com>
2020-11-29 21:23:21 -0500

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -23,7 +23,7 @@ def makeResponse(req):
     parameters = result.get("parameters")
     city = parameters.get("city")
     date = '2020-11-30'
-    r = request.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=031904a6907a7bf1d2f35f31ffc81df6')
+    r = requests.get("http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=031904a6907a7bf1d2f35f31ffc81df6")
     json_object = r.json()
     weather = json_object['list']
     for i in len(weather):
